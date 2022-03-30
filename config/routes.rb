a25d703542188a6e690aaf6341589cc3e3865312
Al Jade <aljade1902@gmail.com>
2022-03-30 22:25:05 +0100

    added change password and forgot password

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
+  # register
   get 'register', to: 'registrations#new'
   post 'register', to: 'registrations#create'
 
+  # login / logout
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  # password reset
+  get 'password', to: 'passwords#edit'
+  patch 'password', to: 'passwords#update'
+
+  # forgot your password
+  get 'password/reset', to: 'password_resets#new'
+  post 'password/reset', to: 'password_resets#create'
+  get 'password/reset/edit', to: 'password_resets#edit'
+  patch 'password/reset/edit', to: 'password_resets#update'
+
 end

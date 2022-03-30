36c9dab43a148d7e9654b5a102d352a391919a18
Al Jade <aljade1902@gmail.com>
2022-03-30 20:47:00 +0100

    added register and login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+
+  get 'register', to: 'registrations#new'
+  post 'register', to: 'registrations#create'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end

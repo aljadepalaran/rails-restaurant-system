0065bab75004859e59b25c3bb080de5163846720
Al Jade <aljade1902@gmail.com>
2022-04-04 22:17:07 +0100

    added accounts and navbar change

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ Rails.application.routes.draw do
   get 'password/reset/edit', to: 'password_resets#edit'
   patch 'password/reset/edit', to: 'password_resets#update'
 
+  # account edit
+  get 'account', to: 'accounts#edit'
+  patch 'account', to: 'accounts#update'
+
   resources :blogs
 
   namespace :admin do
